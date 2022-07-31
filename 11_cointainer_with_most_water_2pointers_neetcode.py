af6b55210d8e4510ea6f6d53d9e7628a74bad7ea
Vedant Manelkar <106972220+vedantcodesig@users.noreply.github.com>
2022-07-31 23:11:40 +0530

    Add files via upload

---
+++ 11_cointainer_with_most_water_2pointers_neetcode.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res=0
+        l,r=0,len(height)-1
+        
+        while l<r:
+            area=(r-l)*min(height[r],height[l])
+            res=max(res,area)
+            
+            if height[l]>height[r]:
+                r-=1
+            else:
+                l+=1
+        return res
---
+++ 11_cointainer_with_most_water_brute_force_neetcode.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+    	res=0
+        for l in range(len(height)):
+            for r in range(l+1,len(height)):
+                area=(r-l)*min(height[l],height[r])
+                res=max(res,area)
+        return res
